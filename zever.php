2ed755390d425ca538c8d1bdc48d37b9066d1c52
B-Mqn <34186424+B-Mqn@users.noreply.github.com>
2019-05-20 00:06:24 +0930

    Update zever.php

--- zever.php
+++ zever.php
@@ -1,62 +1,99 @@
 <?php
-//Zever.php Created by Bman
+//Zever.php Created by Bman - https://github.com/B-Mqn/Zeversolar.php
+//head over to https://forum.pvoutput.org to discuss it.
 
-// 	Manually run command
-//  	 /usr/bin/php /home/pi/zeversolar/zever.php
+// 		-------Manually run command-------
+//  	/usr/bin/php /home/pi/zeversolar/zever.php
 
-//		Cron Script.... 
+//		-------Cron Script-------
 //		crontab -e
-//		*/5 * * * * /usr/bin/php /home/pi/zeversolar/zever.php
+//	*/5 * * * * /usr/bin/php /home/pi/zeversolar/zever.php
 
-// Configuration Options
+// 		-------View Log-------
+//	cat /home/pi/zeversolar/logs/zever_19-05-2019.log			//view log
+//	tail -f /home/pi/zeversolar/logs/zever-19-05-2019.log			//View log Realtime
+//	nano /home/pi/zeversolar/logs/zever-19-05-2019.log			//Edit Log
+
+
+//-------------------------------------------------------------------------------------------------
+// 		-------Configuration Options-------
+
 $dataManagerIP = "192.168.x.xx";							//Inverter IP Address
-$pvOutputApiKEY = "APIKey";                   //PVOutput Api Key found in https://pvoutput.org/account.jsp
-$pvOutputSID = "SystemID";                    //Your pvoutput SystemID
-//															//add Timezone info here??		
+$pvOutputApiKEY = "pvoutput key";							//PvOutput Api Key
+$pvOutputSID = "pvoutput systemid";							//PvOutput System ID
+											//add Timezone info		
 $country = "Australia";
-$capitalCity ="Adelaide";			
-//Settings that can be Used in V7-V12	
-// Inverter					inverterVoltageLive		inverterDCVoltage	inverterHz	inverterACAmps	inverterDCAmps
+$capitalCity ="Adelaide";
 
-$v7 = "phase1Volts";
-$v8 = "phase2Volts";
-$v9 = "phase3Volts";
-$v10 = "phase1Amps";
-$v11 = "phase2Amps";
-$v12 = "phase3Amps";
+//--------------------------------------------------------------------------------------------------
 
 // !!!!!!!!!!!!!!!! DONT EDIT BELOW HERE !!!!!!!!!!!!!!!!!!!
+
+//--------------------------------------------------------------------------------------------------
 // Define Date & Time
 date_default_timezone_set("$country/$capitalCity");
 $system_time= time();
 $date = date('Ymd', time());
 $time = date('H:i', time());
 
-// Inverter API URL
+// Inverter & API URL
 $pvOutputApiURL = "http://pvoutput.org/service/r2/addstatus.jsp?";
 $inverterDataURL = "http://".$dataManagerIP."/home.cgi";
 
 
-//Collect Data From Inverter
-    $context = stream_context_create(array('http'=>array('protocol_version'=>'1.1')));
-    $result = file_get_contents('$dataManagerIP/home.cgi', false, $context);
-    //echo $result;
+$context = stream_context_create(array('http'=>array('protocol_version'=>'1.1')));
+
+
+// x Attepmts at connecting to inverter
+$attempts = 0;
+do {
+  $attempts++;
+  echo "Attempt $attempts\n";
+  $result = file_get_contents($inverterDataURL, false, $context);
+  if (!$result) {
+    echo "Attempt $attempts has failed. Waiting 10 seconds.\n";
+    sleep(10);
+  }
+} while( !$result && $attempts < 5);
+
+
+// Expanding data from api call
+echo $result;
 
 $lines=explode("\n",$result);
 $inverterPowerLive = $lines[10];
 $kwh = $lines[11];
 
 
-//Fix for Zeversolar kWh reading
-$kWh_parts=explode('.', $kWh);
-if (strlen($kWh_parts[1]) == 1) {
-	$kWh_parts[1] = '0' . $kWh_parts[1];
-	$kWh=join('.',$kWh_parts);
-}
-echo $kWh;
+//Echo kWh reading before
+Echo "\n";
+Echo "$kwh \n";
+Echo "\n";
 
-//Convert to Watt for PVOutput
+
+//Addzero if needed & echo result
+$kwh_parts=explode('.', $kwh);
+if (strlen($kwh_parts[1]) == 1) {
+	$kwh_parts[1] = '0' . $kwh_parts[1];
+	$kwh=join('.',$kwh_parts);
+}
+
+Echo "\n";
+Echo "$kwh \n";
+
+
+//Convert kWh into Wh & Echo Result
 $inverterEnergyDayTotal = $kwh * 1000;
+
+if ($kwh > 0) {
+    $inverterEnergyDayTotal = $kwh * 1000;
+} else {
+    $inverterEnergyDayTotal =" ";
+}
+
+Echo "$inverterEnergyDayTotal \n";
+Echo "\n";
+
 
 // Push to PVOutput
 $pvOutputURL = $pvOutputApiURL
@@ -65,32 +102,40 @@ $pvOutputURL = $pvOutputApiURL
                 . "&d=" .   $date
                 . "&t=" .   $time
                 . "&v1=" .  $inverterEnergyDayTotal
-                . "&v2=" .  $inverterPowerLive
-                . "&v6=" .  $inverterVoltageLive
-                . "&v7=" .  $$v7
-                . "&v8=" .  $$v8
-                . "&v9=" .  $$v9
-                . "&v10=" . $$v10
-                . "&v11=" . $$v11
-                . "&v12=" . $$v12;
-file_get_contents(trim($pvOutputURL));													
+                . "&v2=" .  $inverterPowerLive;
+file_get_contents(trim($pvOutputURL));
+
+
 //Print Values to Console
 Echo "\n";
 Echo "d \t $date\n";
 Echo "t \t $time\n";
 Echo "v1 \t $inverterEnergyDayTotal\n";
 Echo "v2 \t $inverterPowerLive\n";
-Echo "v6 \t $inverterVoltageLive\n";
-Echo "v7 \t ${$v7}\n";
-Echo "v8 \t ${$v8}\n";
-Echo "v9 \t ${$v9}\n";
-Echo "v10 \t ${$v10}\n";
-Echo "v11 \t ${$v11}\n";
-Print "v12 \t ${$v12}\n";
 Echo "\n";
 Echo "Sending data to PVOutput.org \n";
 Echo "$pvOutputURL \n";
 Echo "\n";
 
-?>
 
+// Push to log file
+// log file Output ------   attempts to contact inverter, Date, Time, V1, V2   -------
+// using the FILE_APPEND flag to append the content to the end of the file
+
+$file='zever_'.date('d-m-Y').'.log';
+
+$logData = $attempts
+                . "," .  $date
+                . "," .  $time
+                . "," .  $inverterEnergyDayTotal
+                . "," .  $inverterPowerLive;
+//file_put_contents($file, ($logData . "\r\n"), FILE_APPEND);
+file_put_contents("/home/pi/zeversolar/logs/$file", ($logData . "\r\n"), FILE_APPEND);
+
+
+Echo "And output log to $file \n";
+Echo "\n";
+
+
+
+?>
